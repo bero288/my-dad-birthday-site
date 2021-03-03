9530361856f990a9d1efd953c019228496b48114
ebraamwadee <ebraamwadee@gmail.com>
2021-03-03 04:15:46 +0200

    finished the projrct

--- app.js
+++
@@ -1,4 +0,0 @@
-const LOGO = document.querySelectorAll(".logo>path");
-LOGO.forEach((letter) => {
-  console.log(letter.getTotalLength());
-});
---
+++ js/app.js
@@ -0,0 +1,28 @@
+const SVG = document.querySelector(".logo");
+const CAKE = document.querySelector(".cake-container");
+const LAST_LETTER = document.querySelector(".logo .last-letter");
+const BALLONS = document.querySelector(".ballons");
+const SOUND_1 = document.querySelector(".fire-sound");
+const SOUND_2 = document.querySelector(".song");
+LAST_LETTER.addEventListener("animationend", () => {
+  Swal.fire("start?", "there will be a sound!", "question")
+    .then(() => {
+      SOUND_1.play();
+    })
+    .then(() => {
+      BALLONS.classList.add("pop-up");
+    })
+    .then(() => {
+      SVG.classList.add("ended");
+    })
+    .then(() => {
+      setTimeout(() => {
+        CAKE.classList.add("cake-anime");
+      }, 1000);
+    })
+    .then(() => {
+      setTimeout(() => {
+        SOUND_2.play();
+      }, 2000);
+    });
+});
